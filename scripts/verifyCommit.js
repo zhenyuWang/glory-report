13c111642a9ba544222df93acbf28eb7b806ed8e
zhenyuWang <13641039885@163.com>
2021-11-29 20:30:10 +0800

    chore: test

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -1,33 +1,26 @@
-// Invoked on the commit-msg git hook by yorkie.
-import chalk from "chalk";
-import fs from "fs";
-// const chalk = require("chalk");
-// const fs = require("fs");
+const chalk = require("chalk");
 const msgPath = process.env.GIT_PARAMS;
-const msg = fs.readFileSync(msgPath, "utf-8").trim();
+const msg = require("fs").readFileSync(msgPath, "utf-8").trim();
 
 const commitRE =
-  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?(.{1,10})?: .{1,50}/;
-const mergeRe = /^(Merge pull request|Merge branch)/;
+  /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build)(\(.+\))?: .{1,50}/;
 
 if (!commitRE.test(msg)) {
-  if (!mergeRe.test(msg)) {
-    console.log(msg);
-    console.error(
-      `  ${chalk.bgRed.white(" ERROR ")} ${chalk.red(
-        `invalid commit message format.`
-      )}\n\n` +
-        chalk.red(
-          `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
-        ) +
-        `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
-        `    ${chalk.green(
-          `fix(v-model): handle events on blur (close #28)`
-        )}\n\n` +
-        chalk.red(
-          `  See https://github.com/vuejs/vue-next/blob/master/.github/commit-convention.md for more details.\n`
-        )
-    );
-    process.exit(1);
-  }
+  console.error(
+    `  ${chalk.bgRed.white(" ERROR ")} ${chalk.red(
+      `invalid commit message format.`
+    )}\n\n` +
+      chalk.red(
+        `Proper commit message format is required for automated changelog generation. Examples:\n\n`
+      ) +
+      `${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+      `${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n` +
+      chalk.red(`See .github/COMMIT_CONVENTION.md for more details.\n`) +
+      chalk.red(
+        `You can also use ${chalk.cyan(
+          `npm run commit`
+        )} to interactively generate a commit message.\n`
+      )
+  );
+  process.exit(1);
 }
