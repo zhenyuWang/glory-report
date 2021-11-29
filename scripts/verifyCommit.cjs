6b192eac22b13ff51d497c1475b1e2319fb3ddc1
zhenyuWang <13641039885@163.com>
2021-11-29 20:53:50 +0800

    chore: test

--- scripts/verifyCommit.cjs
+++ scripts/verifyCommit.cjs
@@ -1,8 +1,3 @@
-// import chalk from "chalk";
-// import fs from "fs";
-// const msgPath = process.env.GIT_PARAMS;
-// const msg = fs.readFileSync(msgPath, "utf-8").trim();
-
 const chalk = require("chalk");
 const fs = require("fs");
 const msgPath = process.env.GIT_PARAMS;
@@ -12,21 +7,22 @@ const commitRE =
   /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build)(\(.+\))?: .{1,50}/;
 
 if (!commitRE.test(msg)) {
-  console.error(
-    `  ${chalk.bgRed.white(" ERROR ")} ${chalk.red(
-      `invalid commit message format.`
-    )}\n\n` +
-      chalk.red(
-        `Proper commit message format is required for automated changelog generation. Examples:\n\n`
-      ) +
-      `${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
-      `${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n` +
-      chalk.red(`See .github/COMMIT_CONVENTION.md for more details.\n`) +
-      chalk.red(
-        `You can also use ${chalk.cyan(
-          `npm run commit`
-        )} to interactively generate a commit message.\n`
-      )
-  );
+  // console.error(
+  //   `  ${chalk.bgRed.white(" ERROR ")} ${chalk.red(
+  //     `invalid commit message format.`
+  //   )}\n\n` +
+  //     chalk.red(
+  //       `Proper commit message format is required for automated changelog generation. Examples:\n\n`
+  //     ) +
+  //     `${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+  //     `${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n` +
+  //     chalk.red(`See .github/COMMIT_CONVENTION.md for more details.\n`) +
+  //     chalk.red(
+  //       `You can also use ${chalk.cyan(
+  //         `npm run commit`
+  //       )} to interactively generate a commit message.\n`
+  //     )
+  // );
+  console.log("error");
   process.exit(1);
 }
